67b46621c3939224c69f993c787aedeec87e5a2c
Chafardet-1800 <Chafargetanibal@gmail.com>
2024-01-03 15:21:48 -0400

    Cambios de rutas para las imagenes

--- assets/images/images-routes.ts
+++ assets/images/images-routes.ts
@@ -6,99 +6,99 @@
  * COVERS
  */
 export const coverAccessDenied =
-  'common/src/assets/images/covers/chinchin_access_denied.svg';
+  'assets/images/covers/chinchin_access_denied.svg';
 
 export const coverApprovedValidation =
-  'common/src/assets/images/covers/chinchin_approved_validation.svg';
+  'assets/images/covers/chinchin_approved_validation.svg';
 
 export const coverApproved =
-  'common/src/assets/images/covers/chinchin_approved.svg';
+  'assets/images/covers/chinchin_approved.svg';
 
 export const coverPending =
-  'common/src/assets/images/covers/chinchin_pending.svg';
+  'assets/images/covers/chinchin_pending.svg';
 
 export const coverWarning =
-  'common/src/assets/images/covers/chinchin_warning.svg';
+  'assets/images/covers/chinchin_warning.svg';
 
 export const coverRejected =
-  'common/src/assets/images/covers/chinchin_rejected.svg';
+  'assets/images/covers/chinchin_rejected.svg';
 
 export const coverEmptyState =
-  'common/src/assets/images/covers/chinchin_empty_state.png';
+  'assets/images/covers/chinchin_empty_state.png';
 
 export const boAuthCover =
-  'common/src/assets/images/covers/chinchin_bo_auth.jpg';
+  'assets/images/covers/chinchin_bo_auth.jpg';
 
 export const coverNoPrivilege =
-  'common/src/assets/images/covers/chinchin_no_privilege.svg';
+  'assets/images/covers/chinchin_no_privilege.svg';
 
 /**
  * DIALOGS
  */
 
 export const dialogApproved =
-  'common/src/assets/images/icons/chinchin_approved.svg';
+  'assets/images/icons/chinchin_approved.svg';
 
 export const dialogInfo =
-  'common/src/assets/images/icons/chinchin_info.svg';
+  'assets/images/icons/chinchin_info.svg';
 
 export const dialogWarning =
-  'common/src/assets/images/icons/chinchin_warning.svg';
+  'assets/images/icons/chinchin_warning.svg';
 
 export const dialogError =
-  'common/src/assets/images/icons/chinchin_error.svg';
+  'assets/images/icons/chinchin_error.svg';
 
 /**
  * LOGOS
  */
 export const chinchinShapeLogo =
-  'common/src/assets/images/logos/chinchin_shape_logo.png';
+  'assets/images/logos/chinchin_shape_logo.png';
 
 export const chinchinWhiteLogo =
-  'common/src/assets/images/logos/chinchin_white_logo.png';
+  'assets/images/logos/chinchin_white_logo.png';
 
 export const chinchinBlackLogo =
-  'common/src/assets/images/logos/chinchin_black_logo.svg';
+  'assets/images/logos/chinchin_black_logo.svg';
 
 /**
  * ICONS
  */
 export const chinchinMenuIcon =
-  'common/src/assets/images/icons/chinchin_menu.svg';
+  'assets/images/icons/chinchin_menu.svg';
 
 export const chinchinUserIcon =
-  'common/src/assets/images/icons/chinchin_user_circle.svg';
+  'assets/images/icons/chinchin_user_circle.svg';
 
 export const confirmedIcon =
-  'common/src/assets/images/icons/chinchin_confirmed.svg';
+  'assets/images/icons/chinchin_confirmed.svg';
 
 export const pendingIcon =
-  'common/src/assets/images/icons/chinchin_pending.svg';
+  'assets/images/icons/chinchin_pending.svg';
 
 export const missingIcon =
-  'common/src/assets/images/icons/chinchin_missing.svg';
+  'assets/images/icons/chinchin_missing.svg';
 
 export const rejectedIcon =
-  'common/src/assets/images/icons/chinchin_rejected.svg';
+  'assets/images/icons/chinchin_rejected.svg';
 
 export const reversedIcon =
-  'common/src/assets/images/icons/chinchin_reverse.svg';
+  'assets/images/icons/chinchin_reverse.svg';
 
 export const whatsappIcon =
-  'common/src/assets/images/icons/chinchin_icon_whatsapp.svg';
+  'assets/images/icons/chinchin_icon_whatsapp.svg';
 
 export const facebookIcon =
-  'common/src/assets/images/icons/chinchin_icon_facebook.svg';
+  'assets/images/icons/chinchin_icon_facebook.svg';
 
 export const twitterIcon =
-  'common/src/assets/images/icons/chinchin_icon_twitter.svg';
+  'assets/images/icons/chinchin_icon_twitter.svg';
 
 export const xIcon =
-  'common/src/assets/images/icons/chinchin_icon_x.svg';
+  'assets/images/icons/chinchin_icon_x.svg';
 
 export const documentImages = {
-  document_od_png: 'common/src/assets/images/icons/chinchin_od_document.png',
-  document_od_svg: 'common/src/assets/images/icons/chinchin_od_document.svg',
-  upload_file: 'common/src/assets/images/icons/chinchin_od_upload_file.svg',
+  document_od_png: 'assets/images/icons/chinchin_od_document.png',
+  document_od_svg: 'assets/images/icons/chinchin_od_document.svg',
+  upload_file: 'assets/images/icons/chinchin_od_upload_file.svg',
 }
 
