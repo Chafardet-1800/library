cda00de614cbfcc4315b46f07640a7990aa03ca2
GabrielC-Web <gabrieljose.cz21@gmail.com>
2024-04-23 00:21:28 -0400

    ahora el product-card tiene config de estilos

--- components/ecommerce/cmm-product-card/cmm-product-card.component.ts
+++ components/ecommerce/cmm-product-card/cmm-product-card.component.ts
@@ -9,6 +9,16 @@ export class CmmProductCardComponent {
 
   @Input() data: any
 
+  /**
+   * Configuración de la cajita de colores
+   */
+  @Input() config: {
+    colorBoxRounded?: boolean,
+    colorBoxBorder?: boolean
+  } = {
+      colorBoxRounded: false
+    }
+
   choosenVariant: number = 0
 
 }
